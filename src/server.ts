e0df921ed83a59c2d933cb8c68999e893808c31d
Edugon0 <eduardo.vic.goncalves@gmail.com>
2024-12-18 17:14:30 -0300

    Websocket with server and comunication client

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import { Socket } from 'dgram'
+import express, { Application } from 'express'
+import http from 'http'
+import {Server} from 'socket.io'
+
+class App{
+    private app: Application
+    private http: http.Server
+    private io: Server
+    constructor(){
+        this.app = express()
+        this.http = http.createServer(this.app)
+        this.io = new Server(this.http)
+        this.listenSocket()
+        this.setupRoutes()
+    }
+    listenServer(){
+        this.http.listen(3000, () => console.log('server is running in PORT 3000'))
+    }
+    listenSocket(){
+        this.io.on('connection', (socket)=>{
+            console.log('User connect =>',socket.id)
+
+            socket.on('message', (msg)=>{
+                this.io.emit('message', msg)
+            })
+        })
+    }
+    setupRoutes(){
+        this.app.get('/', (req, res)=>{
+            res.sendFile(__dirname + '/index.html')
+        })
+    }
+}
+
+const app = new App()
+
+app.listenServer()
